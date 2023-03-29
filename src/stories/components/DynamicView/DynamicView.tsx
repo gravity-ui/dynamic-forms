2ed032da0917d99f482e590be965e3f08391cbf2
NasgulNexus <mostovoyegor@yandex-team.ru>
2023-03-29 19:00:14 +0200

    fix: moved to a separate dynLink file

--- src/stories/components/DynamicView/DynamicView.tsx
+++ src/stories/components/DynamicView/DynamicView.tsx
@@ -5,28 +5,12 @@ import MonacoEditor from 'react-monaco-editor';
 import {
     AnyObject,
     DynamicView as BaseDynamicView,
-    FormValue,
     Spec,
     dynamicViewConfig,
     prepareSpec,
 } from '../../../lib';
 
-interface DynLinkProps {
-    value: FormValue;
-    link: string;
-}
-
-const DynLink: React.FC<DynLinkProps> = ({value, link}) => (
-    <a
-        href={link}
-        onClick={(e) => {
-            alert(`Link clicked, your text: ${value}`);
-            e.preventDefault();
-        }}
-    >
-        {String(value)}
-    </a>
-);
+import {DynLink} from './DynLink';
 
 export interface DynamicViewProps {
     value: AnyObject;
