75e26d98f57c1d888f536873385623f01db3323e
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-01-25 10:54:00 +0100

    test: playwright action

--- src/lib/kit/constants/common.ts
+++ src/lib/kit/constants/common.ts
@@ -2,4 +2,4 @@ import type {PopoverProps} from '@gravity-ui/uikit';
 
 export const COMMON_POPOVER_PLACEMENT: PopoverProps['placement'] = ['bottom', 'top'];
 
-export const COMMON_TITLE_MAX_WIDTH = 533;
+export const COMMON_TITLE_MAX_WIDTH = 534;
