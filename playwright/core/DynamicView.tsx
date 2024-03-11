1b9c5cdb30338649f5a9fec187e1c2f35061565f
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-03-11 19:26:10 +0100

    test(Playwright): add beforeMount theme and mobile provider

--- playwright/core/DynamicView.tsx
+++ playwright/core/DynamicView.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 
-import {MobileProvider, ThemeProvider} from '@gravity-ui/uikit';
-
 import {DynamicView as DynamicViewBase} from '../../src/lib/core/components/View/DynamicView';
 import {Spec} from '../../src/lib/core/types/specs';
 import {FormValue} from '../../src/lib/core/types/value';
 import {dynamicViewConfig} from '../../src/lib/kit/constants/config';
 
 export const DynamicView = ({spec, value}: {spec: Spec; value: FormValue}) => {
-    return (
-        <ThemeProvider>
-            <MobileProvider>
-                <DynamicViewBase spec={spec} value={value} config={dynamicViewConfig} />
-            </MobileProvider>
-        </ThemeProvider>
-    );
+    return <DynamicViewBase spec={spec} value={value} config={dynamicViewConfig} />;
 };
