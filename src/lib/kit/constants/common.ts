1c10e3c90ee0cfa42bdd2f00cd72c95e4abb1b52
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-08-20 14:28:03 +0200

    fix: styles card and section layouts

--- src/lib/kit/constants/common.ts
+++ src/lib/kit/constants/common.ts
@@ -2,7 +2,7 @@ import type {PopoverProps} from '@gravity-ui/uikit';
 
 export const COMMON_POPOVER_PLACEMENT: PopoverProps['placement'] = ['bottom', 'top'];
 
-export const COMMON_TITLE_MAX_WIDTH = 533;
+export const COMMON_TITLE_MAX_WIDTH = 523;
 
 export const OBJECT_VALUE_PROPERTY_NAME = 'value';
 
