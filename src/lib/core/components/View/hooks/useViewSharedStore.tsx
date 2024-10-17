d1b69cb8f98672423616b40f245d61120dbf21c4
Nikita Zolotykh <58661343+bocembocem@users.noreply.github.com>
2024-10-17 12:20:03 +0200

    feat: add ability to use external values for shared context (#240)

--- src/lib/core/components/View/hooks/useViewSharedStore.tsx
+++ src/lib/core/components/View/hooks/useViewSharedStore.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-export const useViewSharedStore = () => {
-    const [store, setStore] = React.useState({});
+export const useViewSharedStore = (shared?: Record<string, any>) => {
+    const firstRender = React.useRef(true);
+    const [store, setStore] = React.useState(shared || {});
 
     const onChangeShared = React.useCallback(
         (name: string, value: any) => setStore((s) => ({...s, [name]: value})),
         [setStore],
     );
+
+    React.useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+        } else if (shared) {
+            setStore({
+                ...store,
+                ...shared,
+            });
+        }
+    }, [shared]);
 
     return {store, onChangeShared};
 };
