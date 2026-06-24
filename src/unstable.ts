a85da43aafe7dde73a72b4ab34452c20b07bc54c
Egor Mostovoy <mostovoyegor@yandex-team.ru>
2026-06-24 12:26:12 +0200

    chore: add unstable build (#381)

---
+++ src/unstable.ts
@@ -0,0 +1,4 @@
+export * from './lib/unstable/core';
+export {config, untypedConfig} from './lib/unstable/kit/constants';
+export * from './lib/unstable/kit/controls';
+export * from './lib/unstable/kit/wrappers';
